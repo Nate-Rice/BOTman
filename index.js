2b42fd62125c0c91a568740669c41bea79da13a0
unknown <nrice@nmu.edu>
2019-03-24 21:44:48 -0400

    slight refactoring

--- index.js
+++ index.js
@@ -5,64 +5,65 @@ const fs = require("fs");
 bot.commands = new Discord.Collection();
 
 
-fs.readdir("./commands/", (err, files) => { //if file is not found, give error
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
+fs.readdir("./cmds/", (error, cfiles) => { //if file is not found, give error
+  if(error) console.log(error);
+  let jsF = cfiles.filter(file => file.split(".").pop() === "js")
+  if(jsF.length <= 0){
     console.log("Couldn't find commands.");
     return;
   }
 
-  jsfile.forEach((f, i) =>{ //for each file, i = number of files
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
+  jsF.forEach((file, i) =>{ //for each file, i = number of files
+    let cmds = require(`./cmds/${file}`);
+    //console.log(`${file} loaded!`);
+    bot.commands.set(cmds.help.name, cmds);
   });
 });
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is now online`);
+  bot.user.setActivity("I'm Botman");
 });
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+bot.on("message", async msg => {
+  if(msg.author.bot) return; //check if bot is talking to itself
+  if(msg.channel.type === "dm") return; //check if you can Direct Message bot
 
   let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  let splitMsg = msg.content.split(" "); //Decomposing string into an array
+  let cmd = splitMsg[0];
+  let args = splitMsg.slice(1); 
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot, message, args);
+  if(commandfile) commandfile.run(bot, msg, args);
 
 
   if(cmd === `${prefix}serverinfo`){
-    //let sicon = message.guild.displayAvatarURL;
-    let sicon = message.guild.iconURL;
+    //let sicon = msg.guild.displayAvatarURL;
+    let sicon = msg.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
     .setDescription("Server Information")
     .setColor("#db1125")
     .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Server was created on", message.guild.createdAt)
-    .addField("Total members", message.guild.memberCount);
+    .addField("Server Name", msg.guild.name)
+    .addField("Server was created on", msg.guild.createdAt)
+    .addField("Total members", msg.guild.memberCount);
 
-    return message.channel.send(serverembed);
+    return msg.channel.send(serverembed);
   }
 
   if (cmd === `${prefix}user`){
-    let sicon = message.member.user.displayAvatarURL;
+    let sicon = msg.member.user.displayAvatarURL;
     let serverembed = new Discord.RichEmbed()
     .setDescription("User Information")
     .setColor("#db1125")
     .setThumbnail(sicon)
-    .addField("Your Username", message.member.user.username)
-    .addField("Your Nickname", message.member.displayName)
-    .addField("Your Role", message.member.highestRole)
-    .addField("You joined on", message.member.joinedAt);
+    .addField("Your Username", msg.member.user.username)
+    .addField("Your Nickname", msg.member.displayName)
+    .addField("Your Role", msg.member.highestRole)
+    .addField("You joined on", msg.member.joinedAt);
 
-    return message.channel.send(serverembed);
+    return msg.channel.send(serverembed);
   }
 
 });
